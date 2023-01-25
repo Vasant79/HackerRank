be0065cc780127f8ac7b9a88a8c2ecd7c4a6ae29
Vasant Singh Negi <86915073+Vasant79@users.noreply.github.com>
2023-01-25 22:30:37 +0530

    1

---
+++ HackerRank/src/com/solutions/CalanderDemo.java
@@ -0,0 +1,22 @@
+package com.solutions;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+/*
+ * printing day of a given time
+ * always use date and time class java 8 - for more refer javaT
+ */
+public class CalanderDemo {
+
+	public static void main(String[] args) {
+
+		LocalDate ld = LocalDate.of(2023, 01, 23);
+
+		DayOfWeek dow = DayOfWeek.from(ld);
+
+		System.out.println(dow);
+
+	}
+
+}
---
+++ HackerRank/src/com/solutions/CurrencyFormatter.java
@@ -0,0 +1,38 @@
+package com.solutions;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/*
+ * printinf value in term of local currency
+ */
+
+public class CurrencyFormatter {
+
+	public static void main(String[] args) {
+
+		// 1st approch
+		// NumberFormat f = NumberFormat.getCurrencyInstance(Locale.US);
+		// currency = f.format(1000.201);
+
+		System.out.println("Currency");
+
+		// 2nd - one line approch
+		// System.out.println(NumberFormat.getCurrencyInstance(Locale.CHINA).format(12342.97));
+
+		// implementation
+		double payment = 12324.134;
+
+		// creating local for india - no builtin available
+
+		Locale india = new Locale("en", "IN");
+
+		System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+
+		System.out.println(NumberFormat.getCurrencyInstance(india).format(payment));
+
+		System.out.println(NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+		System.out.println(NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+	}
+
+}
